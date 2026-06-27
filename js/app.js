7d548192a3ff7174a04fd2ead1ab617b6a97afe5
pasindudeshan1100-stack <pasindudeshan1100@gmail.com>
2026-06-27 14:56:34 +0530

    Add HTML form elements and initial JavaScript functionality

---
+++ js/app.js
@@ -0,0 +1,35 @@
+ console.log("Hello java Script");
+ let name = "saman"
+ console.log(name);
+ let age=18
+ console.log(age);
+ console.log(name);
+//  alert("hello java")
+ let isAfteral = false;
+ let customers = [];
+ let customer = {}
+ let salary = 7500.00
+console.log(typeof age);
+console.log(typeof customer);
+console.log(typeof customers);
+console.log(typeof isAfteral);
+console.log(typeof salary);
+ 
+let x =100;
+let y =10;
+console.log(x===y);
+
+if(!false){
+    console.log(true);
+}else{
+    console.log(false);
+    
+}
+if(x>y){
+    console.log("x is greater than y");   
+}else{
+    console.log("Y is greater than x");
+}
+
+ 
+ 
