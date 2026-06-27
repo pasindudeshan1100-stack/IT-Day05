c7e8d2837d895bfe805052c82ba5bb9f4733209b
pasindudeshan1100-stack <pasindudeshan1100@gmail.com>
2026-06-27 15:56:08 +0530

    Refactor app.js: Add switch case, while loop, and function for addition

--- js/app.js
+++ js/app.js
@@ -30,6 +30,51 @@ if(x>y){
 }else{
     console.log("Y is greater than x");
 }
+ 
+let day =2;
+switch(day){
+    case 1: console.log("monday")
+    case 2: console.log("tuesday");
+    case 3:console.log("wednesday");
+    default:console.log("Invalid value");   
+}
+ 
+
+
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// -------------------------------------------
 
  
+ let i = 0;
+ while (i <= 10) {
+   console.log(i);
+   i++;
+}
+
+// -------------------------------------------
+
+// do while loop
+
+// let i = 0;
+// do {
+//     console.log(i);
+//     i++;
+// } while (i <= 10);
+
+function add(x,y) {
+    return x+y;  
+}
+let result = add(10,20)
+console.log(result);
+let numbers=[1,"saman",3,true,5,"ranil"]
+for(let i=0;i<5;i++){
+    console.log(numbers[i]);
+    
+}
+
+
+
  
